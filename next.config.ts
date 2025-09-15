7d463c65f44fb81870db26483e8ec353392096be
Ario Triguna <ario.triguna@kitabisa.com>
2025-09-16 02:06:24 +0700

    add wrangler

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,14 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone", // ⛔ kalau mau API routes, ganti jadi "standalone"
+  output: "standalone",
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-console.log(
-  "✅ NEXT Build sees DEEPSEEK_KEY:",
-  process.env.DEEPSEEK_KEY ? "SET" : "MISSING"
-);
+console.log("✅ NEXT Build sees DEEPSEEK_KEY:", process.env.DEEPSEEK_KEY ? "SET" : "MISSING");
 
 export default nextConfig;
